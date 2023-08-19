9db194373cbddf6aca514a1d0110249954081460
yashwardhan kedia <68593331+yashwaerdhan@users.noreply.github.com>
2023-08-19 15:07:22 +0530

    Add files via upload

---
+++ PRO-C163-Student-Activity-main/PRO-C163-Student-Activity-main/playerMovement.js
@@ -0,0 +1,24 @@
+AFRAME.registerComponent("player-movement", {
+    init: function () {
+      this.walk();
+    },
+    walk: function () {
+      window.addEventListener("keydown", (e) => {
+        // Add the condition to play sound
+       if (
+        e.key==="ArrowUp" ||
+        e.key==="ArrowRight" ||
+        e.key==="ArrowLeft" ||
+        e.key==="ArrowDown" 
+      ){
+        var entity=document.querySelector("#sound2")
+        entity.components.sound.playSound()
+      }
+
+
+      });
+    },
+  });
+
+
+  
